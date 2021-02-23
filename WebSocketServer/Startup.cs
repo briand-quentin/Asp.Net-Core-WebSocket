4d4410337814af01373919ea74ad05ede023e174
Briand Quentin <euphoriique@gmail.com>
2021-02-23 11:15:13 +0100

    first commit

---
+++ WebSocketServer/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net.WebSockets;
+
+namespace WebSocketServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        { 
+        }   
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    Console.WriteLine("WebSocket connected");
+                }
+                else
+                {
+                    Console.WriteLine("Hello from the 2nd request delegate");
+                    await next();
+                }
+
+            });
+
+            app.Run(async context => 
+            {
+                Console.WriteLine("Hello from the 3rd request delegate");
+                await context.Response.WriteAsync("Hello from the 3rd request delegate");
+            });
+        }
+    }
+}
