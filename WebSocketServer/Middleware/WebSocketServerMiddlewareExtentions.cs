070d2195eaad962a3bad46b589b0f386c1d72166
Briand Quentin <euphoriique@gmail.com>
2021-02-24 12:07:07 +0100

    DependencyInjection for WebSocket Server Connection Manager

--- WebSocketServer/Middleware/WebSocketServerMiddlewareExtentions.cs
+++ WebSocketServer/Middleware/WebSocketServerMiddlewareExtentions.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 
 namespace WebSocketServer.Middleware
@@ -13,5 +14,11 @@ namespace WebSocketServer.Middleware
         public static IApplicationBuilder UseWebSocketServer(this IApplicationBuilder builder){
             return builder.UseMiddleware<WebSocketServerMiddleware>();
         }
+
+         public static IServiceCollection AddWebSocketManager(this IServiceCollection services){
+             services.AddSingleton<WebSocketServerConnectionManager>();
+             return services;
+        }
+
     }
 }
